c5e02aec39cd0a24eab8216e454a81325bfd645f
AlbertV99 <tico.urquhart@gmail.com>
2023-03-11 18:02:18 -0300

    Creacion de modelo telefono cliente

---
+++ app/Models/TelefonoCliente.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TelefonoCliente extends Model{
+    use HasFactory;
+    public $timestamps = false;
+    protected $table = 'telefono_cliente';
+    protected $fillable = [
+        'id_cliente',
+        'telefono'
+    ];
+}
