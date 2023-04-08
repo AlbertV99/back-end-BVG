351a9ec07537bd3ee596e5e636b2a58e37f5c8d9
AlbertV99 <tico.urquhart@gmail.com>
2023-04-07 22:35:18 -0400

    Creacion de tablas para release2

---
+++ database/migrations/2023_04_08_022159_create_caja_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCajaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('caja', function (Blueprint $table) {
+            $table->id();
+            $table->string('descripcion');
+            $table->string('pin');
+            $table->integer('saldo_actual');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('caja');
+    }
+}
---
+++ database/migrations/2023_04_08_022222_create_aperturas_caja_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAperturasCajaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aperturas_caja', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('caja');
+            $table->foreign('caja')->references('id')->on('caja');
+            $table->unsignedBigInteger('usuario_id');
+            $table->foreign('usuario_id')->references('id')->on('usuario');
+            $table->integer('saldo_apertura');
+            $table->integer('saldo_cierre');
+            $table->date('fecha_apertura');
+            $table->date('fecha_cierre');
+            $table->integer('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aperturas_caja');
+    }
+}
