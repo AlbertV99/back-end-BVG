adfa8e555e5fa44c25b65d9128e864dab78bb153
AlbertV99 <tico.urquhart@gmail.com>
2023-05-07 15:42:19 -0400

    Creacion de seeder y test de menu

---
+++ database/seeders/Menu.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Agrupador;
+use App\Models\OpcionMenu;
+
+
+class Menu extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(){
+        $observacionMenuOpcionDefecto = "CREADO POR SEMILLA";
+        $icono = "testing";
+        $lista = [
+            ["agrupador"=>"Documentos","opciones"=>[
+                ["descripcion"=>"Clientes","direccion"=>"cliente"],
+                ["descripcion"=>"PerfilCliente","direccion"=>"perfilCliente"],
+                ["descripcion"=>"Clientes","direccion"=>"barrio"],
+            ]], #
+            ["agrupador"=>"Credito","opciones"=>[
+                ["descripcion"=>"Tipo Plazo","direccion"=>"tipoPlazo"],
+                ["descripcion"=>"Solicitud Agente","direccion"=>"solicitudAgente"],
+                ["descripcion"=>"Solicitud Analisis","direccion"=>"solicitudAnalista"],
+                ["descripcion"=>"Solicitud Directorio","direccion"=>"solicitudDirectorio"],
+            ]], #
+            ["agrupador"=>"Caja","opciones"=>[
+                ["descripcion"=>"Conceptos","direccion"=>"conceptosCaja"],
+                ["descripcion"=>"Cajas","direccion"=>"caja"],
+                ["descripcion"=>"Movimientos","direccion"=>"operacion"],
+            ]], #
+            ["agrupador"=>"Seguridad","opciones"=>[
+                ["descripcion"=>"Usuarios","direccion"=>"usuario"],
+                ["descripcion"=>"Perfil","direccion"=>"perfilCliente"],
+                ["descripcion"=>"Agrupadores","direccion"=>"agrupador"],
+                ["descripcion"=>"Opciones de Menu","direccion"=>"opcionMenu"],
+            ]], #
+            ["agrupador"=>"Reportes","opciones"=>[]], #
+        ];
+        $estados = [];
+        foreach ($lista as  $value) {
+            $agrupador = Agrupador::create([
+                'descripcion' => $value["agrupador"],
+            ]);
+            foreach ($value["opciones"] as $opcion) {
+                $agrupador->opciones()->save(new OpcionMenu(['descripcion'=>$opcion['descripcion'],"observacion"=>$observacionMenuOpcionDefecto]));
+            }
+        }
+    }
+}
--- tests/Feature/BarrioTest.php
+++ tests/Feature/BarrioTest.php
@@ -6,8 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class BarrioTest extends TestCase
-{
+class BarrioTest extends TestCase{
     /**
      * A basic feature test example.
      *
