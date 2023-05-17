d9fd47cd3c451728700f85f5a2cb51d239eea3be
AlbertV99 <tico.urquhart@gmail.com>
2023-05-16 23:09:35 -0400

    Creacion de controller de documento

---
+++ app/Http/Controllers/DocumentoController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDocumentoRequest;
+use App\Http\Requests\UpdateDocumentoRequest;
+use App\Models\Documento;
+
+class DocumentoController extends Controller{
+    private $c_reg_panel = 25;
+    private $c_reg_lista = 10;
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($pag=0){
+        $c_paginas = ceil(Documento::count()/$this->c_reg_panel);
+        $salto = $pag*$this->c_reg_panel;
+
+        $query = Documento::select("id","nombre","cliente_id","fecha_vencimiento","solicitud_id_contrato","solicitud_id_pagare");
+        // if($busqueda !=""){
+        //     $query = $query->where("usuario.nombre_usuario","like",$busqueda)->orWhere("usuario.nombre","like",$busqueda)->orWhere("usuario.apellido","like",$busqueda)->orWhere("usuario.apellido","like",$busqueda);
+        // }
+        $query = $query->orderBy("nombre");
+
+        return ["cod"=>"00","msg"=>"todo correcto","pagina_actual"=>$pag,"cantidad_paginas"=>$c_paginas,"datos"=>$query->get()];
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreDocumentoRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreDocumentoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Documento  $documento
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Documento $documento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Documento  $documento
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Documento $documento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateDocumentoRequest  $request
+     * @param  \App\Models\Documento  $documento
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateDocumentoRequest $request, Documento $documento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Documento  $documento
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Documento $documento)
+    {
+        //
+    }
+}
