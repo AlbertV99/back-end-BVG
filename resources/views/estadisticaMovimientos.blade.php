5b46cb53cc9a634c211148eb4477249ca40afde3
AlbertV99 <tico.urquhart@gmail.com>
2023-06-20 15:25:09 -0400

    Actualizacion de reporte de estadisticas

--- resources/views/estadisticaMovimientos.blade.php
+++ resources/views/estadisticaMovimientos.blade.php
@@ -2,7 +2,6 @@
 <html>
 <title>B-VG</title>
 <head>
-
     <style>
         table{
             font-family: arial, sans-serif;
@@ -18,52 +17,69 @@
             background-color: #dddddd;
         }
     </style>
-    </head>
+</head>
 
     <body>
         <h1>Reporte estadistico</h1>
         <!-- <p>{{$datos}}</p> -->
 
-        <div width="1000px" height="1000px" style="display: block; box-sizing: border-box; height: 1000px; width: 1000px;">
-            <canvas id="myChart" height="1000px" width="1000px" style="display: block; box-sizing: border-box; height: 1000px; width: 1000px;"></canvas>
+        <div width="500px" height="500px" style="display: block; box-sizing: border-box; height: 500px; width: 500px;">
+            <canvas id="myChart" height="500px" width="500px" style="display: block; box-sizing: border-box; height: 500px; width: 500px;">
+            </canvas>
         </div>
     </body>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script type="text/javascript">
+        // const Datos = JSON.parse("{{json_encode($datos)}}");
+        const Datos = JSON.parse({!!json_encode($datos)!!});
+        console.log(Datos)
 
-    const DATA_COUNT = 7;
-    const NUMBER_CFG = {count: DATA_COUNT, min: -100, max: 100};
+        const DATA_COUNT = 7;
+        const NUMBER_CFG = {count: DATA_COUNT, min: -100, max: 100};
 
-    const labels = ['enero','febrero','marzo','abril','mayo','junio','julio'];
-    const data = {
-        labels: labels,
-        datasets: [
-            {
-                label: 'Dataset 1',
-                data: ['10','11','5','7','16','2','8'],
-                borderColor: 'black',
-                backgroundColor:'white',
-            },
-        ]
-    };
-    const ctx = document.getElementById('myChart');
-    const config = {
-        type: 'line',
-        data: data,
-        options: {
-            responsive: true,
-            plugins: {
-                legend: {
-                    position: 'top',
+        const meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+        const labels = Array.from(new Set(Datos.map((elemento)=>meses[elemento.mes-1])))
+        console.log(labels)
+        const entradas = Datos.map((elemento)=>{if(elemento.tipo=="ENTRADA"){return elemento.total}else{return;} }).filter((elemento)=> typeof elemento != 'undefined')
+        const salidas = Datos.map((elemento)=>{if(elemento.tipo=="SALIDA"){return elemento.total}else{return;} }).filter((elemento)=> typeof elemento != 'undefined')
+
+        console.log({entrada:entradas , salida:salidas})
+
+        const data = {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'ENTRADAS',
+                    data: entradas,
+                    borderColor: 'green',
+                    backgroundColor:'green',
                 },
-                title: {
-                    display: true,
-                    text: 'Chart.js Line Chart'
+                {
+                    label: 'SALIDAS',
+                    data: salidas,
+                    borderColor: 'red',
+                    backgroundColor:'red',
+                },
+            ]
+        };
+        const ctx = document.getElementById('myChart');
+        const config = {
+            type: 'bar',
+            data: data,
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Chart.js Line Chart'
+                    }
                 }
-            }
-        },
-    };
-    new Chart(ctx,config);
+            },
+        };
+        new Chart(ctx,config);
 
     </script>
     </html>
